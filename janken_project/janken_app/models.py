a2547eeda838037ac87de97b01ff53495b2941fe
jimi-117 <sho.jimi117@gmail.com>
2024-01-12 00:38:09 +0100

    set customuser to auth model

--- janken_project/janken_app/models.py
+++ janken_project/janken_app/models.py
@@ -1,43 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.core.validators import MinValueValidator
 
 class CustomUser(AbstractUser):
-    play_num = models.IntegerField(default=0)
-    win_num = models.IntegerField(default=0)
-    lose_num = models.IntegerField(default=0)
-    draw_num = models.IntegerField(default=0)
-    win_rate = models.FloatField(default=0)
-    
-    stone_num = models.IntegerField(default=0)
-    scissors_num = models.IntegerField(default=0)
-    paper_num = models.IntegerField(default=0)
-    
-    image = models.ImageField(default=0)
-    
+    play_num = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+    win_num = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+    lose_num = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+    draw_num = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+    win_rate = models.FloatField(default=0.0)
+
+    # Counts for each hand type
+    stone_num = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+    scissors_num = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+    paper_num = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0)])
+
+    # User profile image
+    image = models.ImageField(upload_to='img/')
+
     def play(self, player_hand, com_hand):
+        # Define the rules in a dictionary to improve readability and maintainability
+        rules = {
+            'stone': 'scissors',
+            'scissors': 'paper',
+            'paper': 'stone',
+        }
+
+        # Calculate the result
         if player_hand == com_hand:
+            self.draw_num += 1
             result = 'draw'
-        elif player_hand == 'stone' and com_hand == 'scissors' or player_hand == 'scissors' and com_hand == 'paper' or player_hand == 'paper' and com_hand == 'stone':
+        elif rules[player_hand] == com_hand:
+            self.win_num += 1
             result = 'win'
         else:
-            result = 'lose'
-            
-        if result == 'draw':
-            self.draw_num += 1
-        elif result == 'win':
-            self.win_num += 1
-        elif result == 'lose':
             self.lose_num += 1
-            
-        if player_hand == 'stone':
-            self.stone_num += 1
-        elif player_hand == 'scissors':
-            self.scissors_num += 1
-        else:
-            self.paper_num += 1
-            
-        self.play_num = self.win_num + self.lose_num + self.draw_num
-        self.win_rate = (self.win_num/self.play_num)*100
-        
+            result = 'lose'
+
+        # Increment hand type count
+        if player_hand in rules:
+            setattr(self, f"{player_hand}_num", getattr(self, f"{player_hand}_num") + 1)
+
+        # Update the play number and win rate
+        self.play_num += 1
+        if self.play_num > 0:
+            self.win_rate = self.win_num / self.play_num * 100
+
+        # Save the changes to the database
+        self.save()
+
         return result
