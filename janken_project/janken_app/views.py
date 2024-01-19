d54df49917fc5626502e52f731fd20eea24632e0
jimi-117 <sho.jimi-117@gmail.com>
2024-01-19 17:15:12 +0100

    end of part 1. ver2.1

--- janken_project/janken_app/views.py
+++ janken_project/janken_app/views.py
@@ -6,21 +6,24 @@ from random import choice
 def index(request):
     return render(request, 'index.html')
 
+# Have to determine game running view func
+def play(request):
+    user_hand = request.GET.get('hand', None)
+    cpu_hand = choice(['rock','scissors','paper'])
+    result = game_rules(user_hand, cpu_hand),
+    print(type(user_hand), type(cpu_hand)),
+    return JsonResponse({'result': result, 'cpu_hand': cpu_hand})
+
 # Janken rules func
 def game_rules(user_hand, cpu_hand):
+    win_cases = [('rock', 'scissors'), ('scissors', 'paper'), ('paper', 'rock')]
+    
     if user_hand == cpu_hand:
         return 'draw'
-    elif (user_hand == 'rock' and cpu_hand == 'scissors') or \
-            (user_hand == 'scissors' and cpu_hand == 'paper') or \
-            (user_hand == 'paper' and cpu_hand == 'rock'):
+    elif (user_hand, cpu_hand) in win_cases:
         return 'win'
     else:
         return 'lose'
     
-# Have to determine game running view func
-def play(request):
-    user_hand = request.GET.get('hand', None),
-    cpu_hand = choice(['rock','scissors','paper'])
-    result = game_rules(user_hand, cpu_hand),
-    return JsonResponse({'result': result, 'cpu_hand': cpu_hand})
+
 
